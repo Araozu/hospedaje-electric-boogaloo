c6d41a7557c8fe76169b5f451a0236edf62d4333
Fernando Araoz <fernando@araoz.dev>
2026-08-24 16:03:09 -0500

    begin work on new reservation ui

--- src/components/reservas/reservas-page.tsx
+++ src/components/reservas/reservas-page.tsx
@@ -1,5 +1,13 @@
 import { FilterContainer } from "@/components/ui/filter-container";
 import {
+	Popover,
+	PopoverContent,
+	PopoverDescription,
+	PopoverHeader,
+	PopoverTitle,
+	PopoverTrigger,
+} from "@/components/ui/popover"
+import {
 	addCalendarDays,
 	floors,
 	getReservationDays,
@@ -8,6 +16,9 @@ import {
 } from "@/data/reservas";
 
 import { FloorReservationView } from "./floor-reservation-view";
+import { Button } from "../ui/button";
+import { PlusIcon } from "lucide-react";
+import { Calendar } from "../ui/calendar";
 
 export function Reservas() {
 	const weekStart = getWeekStart(new Date());
@@ -22,25 +33,28 @@ export function Reservas() {
 			<h1 className="text-2xl font-heading font-semibold tracking-tight mb-4">
 				Reservas
 			</h1>
-			<div className="flex gap-4">
-				<span className="font-heading">Tipo:</span>
-				<FilterContainer
-					allText="Todos"
-					options={[
-						{ label: "doble", text: "Doble" },
-						{ label: "matrimonial", text: "Matrimonial" },
-						{ label: "deluxe", text: "Suite Deluxe" },
-					]}
-				/>
+			<div className="grid grid-cols-[auto_10rem] gap-4">
+				<div className="flex gap-4">
+					<span className="font-heading">Tipo:</span>
+					<FilterContainer
+						allText="Todos"
+						options={[
+							{ label: "doble", text: "Doble" },
+							{ label: "matrimonial", text: "Matrimonial" },
+							{ label: "deluxe", text: "Suite Deluxe" },
+						]}
+					/>
 
-				<span className="font-heading">Disponibles:</span>
-				<FilterContainer
-					allText="Todo"
-					options={[
-						{ label: "now", text: "Hoy" },
-						{ label: "week", text: "Semana" },
-					]}
-				/>
+					<span className="font-heading">Disponibles:</span>
+					<FilterContainer
+						allText="Todo"
+						options={[
+							{ label: "now", text: "Hoy" },
+							{ label: "week", text: "Semana" },
+						]}
+					/>
+				</div>
+				<NewReservation />
 			</div>
 			<div className="mt-6">
 				{floors.map((f) => (
@@ -55,4 +69,23 @@ export function Reservas() {
 			</div>
 		</div>
 	);
+}
+
+function NewReservation() {
+	return (
+		<Popover>
+			<PopoverTrigger render={<Button />}>
+				<PlusIcon strokeWidth={3} />
+				Nueva reserva
+			</PopoverTrigger>
+			<PopoverContent align="end">
+				<PopoverHeader>
+					<PopoverTitle>Fechas</PopoverTitle>
+					<PopoverDescription>Selecciona una fecha:</PopoverDescription>
+
+					<Calendar className="w-full" mode="range" />
+				</PopoverHeader>
+			</PopoverContent>
+		</Popover>
+	)
 }
